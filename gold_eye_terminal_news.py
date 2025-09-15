242d0ae521a8cd00a3582905614c3af456ce3894
Umer Farid <57315483+umer-farid@users.noreply.github.com>
2025-09-15 19:12:29 +0500

    Update gold_eye_terminal_news.py

--- gold_eye_terminal_news.py
+++ gold_eye_terminal_news.py
@@ -3,13 +3,12 @@ import requests
 from xml.etree import ElementTree as ET
 from urllib.parse import urlparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
-#from streamlit_autorefresh import st_autorefresh
+from streamlit_autorefresh import st_autorefresh
 from datetime import datetime
 import pytz
 import plotly.express as px
 import pandas as pd
 from dateutil import parser
-import time
 
 # -----------------------
 # Page Config & Header
@@ -23,18 +22,7 @@ st.markdown("<p style='text-align: center; font-style: italic; color: orange;'>â
 # -----------------------
 st.sidebar.header("Controls")
 slow_refresh = st.sidebar.number_input("News refresh interval (seconds)", 30, 600, 60)
-# Refresh interval (seconds)
-REFRESH_INTERVAL = slow_refresh*1000  
-
-# Initialize timestamp in session_state
-if "last_refresh" not in st.session_state:
-    st.session_state["last_refresh"] = time.time()
-
-# If time since last refresh exceeds interval â†’ rerun
-if time.time() - st.session_state["last_refresh"] > REFRESH_INTERVAL:
-    st.session_state["last_refresh"] = time.time()
-    st.experimental_rerun()
-#st_autorefresh(interval=slow_refresh*1000, key="refresh")
+st_autorefresh(interval=slow_refresh*1000, key="refresh")
 
 tz = pytz.timezone("Asia/Karachi")
 def _ts(): return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
@@ -349,5 +337,3 @@ with r1c3:
         time_str = local_dt.strftime("%Y-%m-%d %H:%M:%S")
         lines.append(f"[{time_str}] <a href='{it['link']}' target='_blank'>{it['text']}</a>")
     st.markdown("<div class='small-terminal'>" + "<br>".join(lines) + "</div>", unsafe_allow_html=True)
-
-
